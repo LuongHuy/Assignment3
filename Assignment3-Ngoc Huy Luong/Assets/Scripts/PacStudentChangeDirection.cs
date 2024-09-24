2330c994001258d4069854b9cd15053a6e049963
HuyLuong <ngochuybu@gmail.com>
2024-09-24 23:22:09 +1000

    Movement Sync with Anim

--- Assignment3-Ngoc Huy Luong/Assets/Scripts/PacStudentChangeDirection.cs
+++ Assignment3-Ngoc Huy Luong/Assets/Scripts/PacStudentChangeDirection.cs
@@ -8,13 +8,12 @@ public class PacStudentChangeDirection : MonoBehaviour
 {
     public Animator animatorController;
     public bool IsDead;
-
     private float changeDirectionInterval = 1.38f;
-    private string[] directions = { "right", "down", "left", "up" };
-    private int currentDirectionIndex = 0;
+    private PacMovementControl pacMovementControl;
 
     void Start()
     {
+        pacMovementControl = GetComponent<PacMovementControl>(); 
         animatorController.SetBool("isDead?", IsDead);
         StartCoroutine(ChangeDirectionCoroutine());
     }
@@ -30,9 +29,19 @@ public class PacStudentChangeDirection : MonoBehaviour
         {
             if (!IsDead)
             {
-                Debug.Log($"Changing direction to: {directions[currentDirectionIndex]}");
-                animatorController.SetTrigger(directions[currentDirectionIndex]);
-                currentDirectionIndex = (currentDirectionIndex + 1) % directions.Length;
+                Vector3 currentDirection = pacMovementControl.CurrentDirection;
+
+              
+                if (Mathf.Abs(currentDirection.x) > Mathf.Abs(currentDirection.y))
+                {
+                    animatorController.SetBool("IsWalkLeft", currentDirection.x < 0);
+                    animatorController.SetBool("IsWalkRight", currentDirection.x > 0);
+                }
+                else // Vertical movement
+                {
+                    animatorController.SetBool("IsWalkUp", currentDirection.y > 0);
+                    animatorController.SetBool("IsWalkDown", currentDirection.y < 0);
+                }
             }
             yield return new WaitForSeconds(changeDirectionInterval);
         }
