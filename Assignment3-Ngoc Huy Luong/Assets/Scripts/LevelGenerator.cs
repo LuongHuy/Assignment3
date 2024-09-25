19a5f753bddc5718cf39e5d269ccb3d1c4a5f578
HuyLuong <ngochuybu@gmail.com>
2024-09-26 01:03:43 +1000

    LevelGenerator

--- Assignment3-Ngoc Huy Luong/Assets/Scripts/LevelGenerator.cs
+++ Assignment3-Ngoc Huy Luong/Assets/Scripts/LevelGenerator.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    int[,] levelMap =
-{
+    private int[,] levelMap = new int[,]
+{ 
 {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
 {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
 {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
@@ -22,13 +22,47 @@ public class LevelGenerator : MonoBehaviour
 {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
 {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
 };
-    // Start is called before the first frame update
+ 
     void Start()
     {
-        
+        Destroy(GameObject.Find("Level1"));
+        GenerateLevel();
     }
 
-    // Update is called once per frame
+    void GenerateLevel()
+    {
+        for (int x = 0; x < levelMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < levelMap.GetLength(1); y++)
+            {
+                int tileType = levelMap[x, y];
+                Vector3 position = new Vector3(x, y, 0);
+                Quaternion rotation = Quaternion.identity;
+                GameObject tilePrefab = GetPrefabFromType(tileType);
+                GameObject tile = Instantiate(tilePrefab, position, rotation);
+                tile.transform.SetParent(transform);
+            }
+        }
+    }
+
+    GameObject GetPrefabFromType(int type)
+    {
+        switch (type)
+        {
+           /* case 0: return null; 
+            case 1: return outCornerPrefab; 
+            case 2: return outWallPrefab; 
+            case 3: return innerCornerPrefab; 
+            case 4: return innerWallPrefab; 
+            case 5: return standardSpacePrefab; 
+            case 6: return specialSpacePrefab;
+            case 7: return tWallPrefab; */
+            default: return null;
+        }
+    }
+
+
+
     void Update()
     {
         
