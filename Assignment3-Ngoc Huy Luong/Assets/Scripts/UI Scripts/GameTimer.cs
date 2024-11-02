31c862f687622c107d60f7cf0e9cdba3dad0ee9c
HuyLuong <ngochuybu@gmail.com>
2024-11-03 04:09:37 +1100

    Update Scene

---
+++ Assignment3-Ngoc Huy Luong/Assets/Scripts/UI Scripts/GameTimer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText; 
+    private float timeElapsed;
+    private bool isRunning;
+
+    void Start()
+    {
+        timeElapsed = 0f;
+        isRunning = true; 
+        UpdateTimerText(); 
+    }
+
+    void Update()
+    {
+        if (isRunning)
+        {
+            timeElapsed += Time.deltaTime; 
+            UpdateTimerText(); 
+        }
+    }
+
+    void UpdateTimerText()
+    {
+        int hours = (int)(timeElapsed / 3600);
+        int minutes = (int)((timeElapsed % 3600) / 60);
+        int seconds = (int)(timeElapsed % 60);
+        timerText.text = "Timer: " + (hours < 10 ? "0" + hours : hours.ToString()) + ":" +
+                         (minutes < 10 ? "0" + minutes : minutes.ToString()) + ":" +
+                         (seconds < 10 ? "0" + seconds : seconds.ToString());
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        timeElapsed = 0f;
+        UpdateTimerText();
+    }
+}
+
